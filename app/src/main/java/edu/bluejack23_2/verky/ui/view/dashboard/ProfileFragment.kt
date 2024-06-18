8dadf4928b54993334ef503a453e98b3e7c0f74b
Jonathan Maverick <jonathanmaverick.jm@gmail.com>
2024-06-18 13:13:56 +0700

    fetch profile page

--- app/src/main/java/edu/bluejack23_2/verky/ui/view/dashboard/ProfileFragment.kt
+++ app/src/main/java/edu/bluejack23_2/verky/ui/view/dashboard/ProfileFragment.kt
@@ -2,10 +2,14 @@ package edu.bluejack23_2.verky.ui.view.dashboard
 
 import androidx.fragment.app.viewModels
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
 import edu.bluejack23_2.verky.R
 import edu.bluejack23_2.verky.ui.viewmodel.ProfileViewModel
 
@@ -19,7 +23,7 @@ class ProfileFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        viewModel.loadProfile();
         // TODO: Use the ViewModel
     }
 
@@ -27,6 +31,19 @@ class ProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+        setupObservers(view);
+        return view;
+    }
+
+    private fun setupObservers(view: View) {
+        viewModel.profileData.observe(viewLifecycleOwner) { profile ->
+            Log.e("test", profile.name)
+            view.findViewById<TextView>(R.id.ProfileName).text = profile.name
+            val profileImageView = view.findViewById<ImageView>(R.id.ProfilePicture)
+            Glide.with(this)
+                .load(profile.profilePicture)
+                .into(profileImageView)
+        }
     }
 }

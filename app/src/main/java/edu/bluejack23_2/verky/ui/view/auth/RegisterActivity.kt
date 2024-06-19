5c43b3c215ab54c2816b353e37795699da22b12b
Vincent Ramaputra <vincentramaputra123@gmail.com>
2024-06-19 13:20:03 +0700

    test

--- app/src/main/java/edu/bluejack23_2/verky/ui/view/auth/RegisterActivity.kt
+++ app/src/main/java/edu/bluejack23_2/verky/ui/view/auth/RegisterActivity.kt
@@ -4,17 +4,21 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import edu.bluejack23_2.verky.R
 import edu.bluejack23_2.verky.databinding.ActivityRegisterBinding
+import edu.bluejack23_2.verky.databinding.FragmentStep1RegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityRegisterBinding;
 
     private lateinit var step1Fragment : Step1RegisterFragment
-    private lateinit var step2Fragment : Step1RegisterFragment
+    private lateinit var step2Fragment : Step2RegisterFragment
     private lateinit var step3Fragment : Step1RegisterFragment
+
+    private lateinit var step1FragmentBinding : FragmentStep1RegisterBinding
 
     fun init() {
         step1Fragment = Step1RegisterFragment()
+        step2Fragment = Step2RegisterFragment()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,8 +28,11 @@ class RegisterActivity : AppCompatActivity() {
 
         init()
 
-        supportFragmentManager.beginTransaction().replace(R.id.registerFragmentContainer, step1Fragment).commit()
+        supportFragmentManager.beginTransaction().replace(R.id.registerFragmentContainer, step1Fragment).addToBackStack(null).commit()
 
+//        step1FragmentBinding.continueButton.setOnClickListener{
+//            supportFragmentManager.beginTransaction().replace(R.id.registerFragmentContainer, step2Fragment).addToBackStack(null).commit()
+//        }
 
     }
 
--- app/src/main/java/edu/bluejack23_2/verky/ui/view/auth/Step1RegisterFragment.kt
+++ app/src/main/java/edu/bluejack23_2/verky/ui/view/auth/Step1RegisterFragment.kt
@@ -5,11 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.replace
 import edu.bluejack23_2.verky.R
+import edu.bluejack23_2.verky.databinding.FragmentStep1RegisterBinding
 
 class Step1RegisterFragment : Fragment() {
 
-
+    private lateinit var binding : FragmentStep1RegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 

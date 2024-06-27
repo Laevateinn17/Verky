9c25a0cc3229f8659f2d9452f520a7a1345cfffd
Vincent Ramaputra <vincentramaputra123@gmail.com>
2024-06-27 14:41:32 +0700

    updated

---
+++ app/src/main/java/edu/bluejack23_2/verky/ui/view/dashboard/publicprofilefragment/PublicMyPhotoFragment.kt
@@ -0,0 +1,30 @@
+package edu.bluejack23_2.verky.ui.view.dashboard.publicprofilefragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import edu.bluejack23_2.verky.R
+
+
+class PublicMyPhotoFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_public_my_photo, container, false)
+    }
+
+
+}

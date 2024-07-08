bb6493de69835289f0100cf85bfc97a99a2b340d
Vincent Ramaputra <vincentramaputra123@gmail.com>
2024-07-08 14:29:56 +0700

    chat gacor

--- app/src/main/java/edu/bluejack23_2/verky/ui/adapter/MessageAdapter.kt
+++ app/src/main/java/edu/bluejack23_2/verky/ui/adapter/MessageAdapter.kt
@@ -3,20 +3,49 @@ package edu.bluejack23_2.verky.ui.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import edu.bluejack23_2.verky.R
+import edu.bluejack23_2.verky.data.model.LoggedUser
 import edu.bluejack23_2.verky.data.model.Message
+import java.util.Calendar
+import kotlin.time.Duration.Companion.hours
+import kotlin.time.Duration.Companion.minutes
 
 class MessageAdapter : ListAdapter<Message, MessageAdapter.MessageViewHolder>(MessageDiffCallback()) {
 
     inner class MessageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val contentTextView: TextView = itemView.findViewById(R.id.chat_item_name)
+        private val receiverTextContainer: LinearLayout = itemView.findViewById(R.id.receiver_text)
+        private val senderTextContainer: LinearLayout = itemView.findViewById(R.id.sender_text)
+        private val receiverTextView: TextView = itemView.findViewById(R.id.receiver_chat_item)
+        private val senderTextView: TextView = itemView.findViewById(R.id.sender_chat_item)
+        private val receiverTimeTextView: TextView = itemView.findViewById(R.id.receiver_time_stamp)
+        private val senderTimeTextView: TextView = itemView.findViewById(R.id.sender_time_stamp)
+        private fun formatTimestamp(timestamp: Long?): String {
+            timestamp?.let {
+                val calendar = Calendar.getInstance().apply { timeInMillis = it }
+                val hours = calendar.get(Calendar.HOUR)
+                val minutes = calendar.get(Calendar.MINUTE)
+                val period = if (calendar.get(Calendar.AM_PM) == Calendar.AM) "AM" else "PM"
+                return String.format("%02d:%02d %s", if (hours == 0) 12 else hours, minutes, period)
+            }
+            return ""
+        }
 
         fun bind(message: Message) {
-            contentTextView.text = message.content
+            if (message.userID == LoggedUser.getInstance().getUser()?.id) {
+                senderTextView.text = message.content
+                senderTimeTextView.text = formatTimestamp(message.timestamp)
+                receiverTextContainer.visibility = View.GONE
+            }
+            else {
+                receiverTextView.text = message.content
+                receiverTimeTextView.text = formatTimestamp(message.timestamp)
+                senderTextContainer.visibility = View.GONE
+            }
         }
     }
 

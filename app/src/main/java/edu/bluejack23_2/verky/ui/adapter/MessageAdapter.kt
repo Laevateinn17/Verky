47f4dee4b0d48efae2813c56ad3276787f586175
Vincent Ramaputra <vincentramaputra123@gmail.com>
2024-07-10 07:51:51 +0700

    fix chat

--- app/src/main/java/edu/bluejack23_2/verky/ui/adapter/MessageAdapter.kt
+++ app/src/main/java/edu/bluejack23_2/verky/ui/adapter/MessageAdapter.kt
@@ -12,8 +12,6 @@ import edu.bluejack23_2.verky.R
 import edu.bluejack23_2.verky.data.model.LoggedUser
 import edu.bluejack23_2.verky.data.model.Message
 import java.util.Calendar
-import kotlin.time.Duration.Companion.hours
-import kotlin.time.Duration.Companion.minutes
 
 class MessageAdapter : ListAdapter<Message, MessageAdapter.MessageViewHolder>(MessageDiffCallback()) {
 
@@ -24,6 +22,7 @@ class MessageAdapter : ListAdapter<Message, MessageAdapter.MessageViewHolder>(Me
         private val senderTextView: TextView = itemView.findViewById(R.id.sender_chat_item)
         private val receiverTimeTextView: TextView = itemView.findViewById(R.id.receiver_time_stamp)
         private val senderTimeTextView: TextView = itemView.findViewById(R.id.sender_time_stamp)
+
         private fun formatTimestamp(timestamp: Long?): String {
             timestamp?.let {
                 val calendar = Calendar.getInstance().apply { timeInMillis = it }
@@ -39,11 +38,12 @@ class MessageAdapter : ListAdapter<Message, MessageAdapter.MessageViewHolder>(Me
             if (message.userID == LoggedUser.getInstance().getUser()?.id) {
                 senderTextView.text = message.content
                 senderTimeTextView.text = formatTimestamp(message.timestamp)
+                senderTextContainer.visibility = View.VISIBLE
                 receiverTextContainer.visibility = View.GONE
-            }
-            else {
+            } else {
                 receiverTextView.text = message.content
                 receiverTimeTextView.text = formatTimestamp(message.timestamp)
+                receiverTextContainer.visibility = View.VISIBLE
                 senderTextContainer.visibility = View.GONE
             }
         }
